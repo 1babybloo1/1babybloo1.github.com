1f2b483ccda06a3f117fb439863be15810966e3d
bloo <trixdesignsofficial@gmail.com>
2025-06-10 13:22:21 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    const fadeElems = document.querySelectorAll('.fade-in');
+
+    const appearOptions = {
+        threshold: 0,
+        rootMargin: "0px 0px -100px 0px"
+    };
+
+    const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return;
+            } else {
+                entry.target.classList.add('visible');
+                appearOnScroll.unobserve(entry.target);
+            }
+        });
+    }, appearOptions);
+
+    fadeElems.forEach(elem => {
+        appearOnScroll.observe(elem);
+    });
+
+});
